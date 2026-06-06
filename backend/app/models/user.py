7faeae298097b193e836ca5de0aa85357217065f
Nguyễn Văn Minh Thủy <thuynvm1610@gmail.com>
2026-06-06 14:23:42 +0700

    bugfix: user's email without unique constraint

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -21,6 +21,7 @@ class User(Base):
     email: Mapped[str] = mapped_column(
         String(255),
         nullable=False,
+        unique=True,
     )
     hashed_password: Mapped[str] = mapped_column(
         String(255),
